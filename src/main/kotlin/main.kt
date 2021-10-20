c5688138afba43a086d40b7ea3fa78d1f73485ab
lagashinani <lagashina.ni@sch2033.ru>
2021-10-20 12:57:15 +0300

    init

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,11 @@
 package ru.netology
 fun main() {
-    var likes = 28
-    println("Понравилось $likes человек")
+    var likes = 0
+    if (likes==1) {
+        println("Понравилось $likes человеку")
+    }
+    else {
+        println("Понравилось $likes людям")
+    }
 
 }
